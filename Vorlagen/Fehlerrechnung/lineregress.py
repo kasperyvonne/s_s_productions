14b3bc1fad8ae9ae1db53bb96f9f91adb4832adf
Stefan <stefan.grisard@tu-dortmund.de>
2016-10-17 18:12:59 +0200

    Added: lineregress.py Enthält Funktion, die die Koeffizienten für eine Ausgleichsfunktion der Gestalt y  = A*x + B ermittelt.

---
+++ Vorlagen/Fehlerrechnung/lineregress.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Lineare Regression mittels Methode der kleinsten Quadrate
+def linregress(x, y):
+    assert len(x) == len(y)
+
+    x, y = np.array(x), np.array(y)
+
+    N = len(y)
+    Delta = N * np.sum(x**2) - (np.sum(x))**2
+
+    # A ist die Steigung, B der y-Achsenabschnitt
+    A = (N * np.sum(x * y) - np.sum(x) * np.sum(y)) / Delta
+    B = (np.sum(x**2) * np.sum(y) - np.sum(x) * np.sum(x * y)) / Delta
+
+    sigma_y = np.sqrt(np.sum((y - A * x - B)**2) / (N - 2))
+
+    A_error = sigma_y * np.sqrt(N / Delta)
+    B_error = sigma_y * np.sqrt(np.sum(x**2) / Delta)
+
+    return A, A_error, B, B_error
