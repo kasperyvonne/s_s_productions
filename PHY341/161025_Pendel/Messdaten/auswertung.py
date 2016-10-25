b738a677afe3f23133c00fa3e11541f242ec2e8f
Stefan <stefan.grisard@tu-dortmund.de>
2016-10-25 20:35:53 +0200

    auswertung.py bearbeitet

--- PHY341/161025_Pendel/Messdaten/auswertung.py
+++ PHY341/161025_Pendel/Messdaten/auswertung.py
@@ -3,6 +3,13 @@ import numpy as np
 
 # Zunächst für die Länge l = 0.7 m
 
-t_links = 0.2 * np.genfromtxt('T_Pendel_links_70.txt', unpack = True)
+t_links = np.genfromtxt('T_Pendel_links_70.txt', unpack = True)
 t_links_mid = np.mean(t_links)
-t_links_std = np.
+t_links_std = 1/np.sqrt(len(t_links)) * np.std(t_links)
+print(t_links_mid, t_links_std)
+
+
+t_rechts = np.genfromtxt('T_Pendel_rechts_70.txt', unpack = True)
+t_rechts_mid = np.mean(t_rechts)
+t_rechts_std = 1/np.sqrt(len(t_rechts)) * np.std(t_rechts)
+print(t_rechts_mid, t_rechts_std)
