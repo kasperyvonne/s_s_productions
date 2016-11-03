1d5a390ed60536a28f7a77852ebaa81b7958e11b
Stefan <stefan.grisard@tu-dortmund.de>
2016-11-03 20:08:35 +0100

    Neues Projekt erstellt

---
+++ PHY341/161108_Trägheitsmoment/Messdaten/input.py
@@ -0,0 +1,70 @@
+import numpy as np
+import sys
+from collections import OrderedDict
+
+print('Dateinamen eingeben (ohne .txt): ')
+dateiname = input()
+
+print('Anzahl der Messgrößen eingeben')
+p = int(input())
+data = OrderedDict()
+
+
+head = ""
+for i in range(0, p):
+    print(i+1, 'te Messgröße eingeben')
+    v = input()
+    data[v] = []
+    head += v
+    head += " "
+
+print(head)
+print('Zeilenweise die Messwerte eingeben: ')
+u = 'a'
+while u != 'x':
+    for k in data:
+        u = input()
+        if u == 'x':
+            break
+        data[k].append(float(u))
+        print(data[k])
+
+np.savetxt(dateiname + '.txt', np.column_stack(data.values()), header = head)
+
+
+
+
+
+with open(dateiname + '.tex', 'w') as f:
+
+
+    f.write('\\begin{minipage}{0.49\textwidth} \n \\centering \n \\begin{tabular}{')
+    f.write(p*'S ')
+    f.write('} \n \\toprule \\\ \n')
+
+    helpindex = 0
+    for key in data.keys():
+        if helpindex == p-1:
+            f.write('$'+ key + '$ \\\ \n')
+        else:
+            f.write('$' + key + '$  & ')
+        helpindex +=1
+
+
+
+
+    f.write('\\midrule \\\ \n ')
+
+    rowCount = len(data[v])
+    for j in range(0, rowCount):
+        helpindex = 0
+        for i in data:
+
+            if helpindex == p-1:
+                f.write('{:.2f} \\\ \n '.format(data[i][j]))
+            else:
+                f.write('{:.2f} & '.format(data[i][j]))
+            helpindex += 1
+
+
+    f.write('\\bottomrule \n \\end{tabular} \n \\captionof{table}{...} \n \\label{tab:...} \n  \\end{minipage}')
