3401f63ec9acfd92dc7fc5b4830fd3c2bae046de
beckstev <beckstev@web.de>
2016-11-11 15:12:58 +0100

    Auswertung Teil 4

--- PHY341/161108_Trägheitsmoment/Messdaten/auswertung.py
+++ PHY341/161108_Trägheitsmoment/Messdaten/auswertung.py
@@ -9,45 +9,11 @@ import matplotlib.pyplot as plt
 
 
 abstand, schwingungsdauer = np.genfromtxt('eigenmoment.txt',unpack=True)
+winkelrecht_passiv_wert,winkelrecht_passiv_fehler =np.genfromtxt('Winkelrichtgröße_passiv.txt',unpack=True)
+winkelrecht_passiv=ufloat(winkelrecht_passiv_wert,winkelrecht_passiv_fehler)
+schingungsdauer_grauer_zylinder=np.genfromtxt('T_zylinder_gross.txt',unpack=True)
+schwingungsdauer_kugel=np.genfromtxt('T_grosse_kugel.txt',unpack=True)
 
-
-
-print('Bitte Intervalllänge angeben: ')
-
-
-m=int(input())
-
-
-
-g_1= [] 
-m_u1= []
-m_f=[] 
-f_f=[] 
-v_m=[]
-v_f=[]
-dopp=[]
-dopp_f=[]
-n =0
-a=0
-b=13e-2
-b_f=1e-3
-
-#while n<len(u):
-#	l=sum(u[n:n+m])
-#	m_u=l/m
-#	m_u1.append(m_u)
-#	print('Der Mittelwert lautet: ', m_u)
-#	hilfsv=0
-#	n+=m
-#	while a<n:
-#		hilfsv+=(u[a]-m_u)**2
-#		a+=1
-#	m_b=np.sqrt(1/(m*(m-1))*hilfsv)
-#	m_f.append(m_b)	
-#	m_un=unp.uarray(m_u,m_b)
-#	print('Dabei ist der Fehler des Mittelwert: ',m_b)
-#	print('\n')
-	
 #Berechnung von  D pasiv:
 
 #def D(rad, wink, kraf):
@@ -76,14 +42,14 @@ b_f=1e-3
 hoehe_zylinder=0.5*3.49
 abstand+=hoehe_zylinder
 abstand*=10e-2
-schwingungsdauer*=0.2
+schwingungsdauer*=(1/5)
 schwingungsdauer_hilf=[]
 
-print(abstand[::3])
+#print(abstand[::3])
 for i in range(len(schwingungsdauer))[::3]:
     schwingungsdauer_hilf.append(np.mean(schwingungsdauer[i:i+3]))
 schwingungsdauer_mittel=np.array(schwingungsdauer_hilf)
-print('Zeitlichesmittel ', schwingungsdauer_mittel**2)
+#print('Zeitlichesmittel ', schwingungsdauer_mittel**2)
 
 
 def f(m,u,b):
@@ -113,20 +79,56 @@ def linregress(x, y):
 
 m,m_e,b,b_e=linregress(abstand[::3]**2,schwingungsdauer_mittel**2)
 
+m_fehler=ufloat(m,m_e)
+b_fehler=ufloat(b,b_e)
+print('Steigung ', m_fehler)
+m_1=222.51e-3
+m_2=223.46e-3
+winkelrichtgroesse=(((m_1+m_2)*4*np.pi**2)/m_fehler)
+print('Winkelrichtgroesse ', winkelrichtgroesse)
 
+
+
+#Berechnung vom Eigenträgheitsmoment
+
+def J_zylinder(radius,hoehe,masse):
+	return (1/4)*masse*radius**2+(1/12)*masse*hoehe**2
+
+radius_zylinder=(3.49*0.5)*10**(-3)
+hoehe_zylinder=(14.05*0.5)*10**(-3)
+J_zylinder1=J_zylinder(radius_zylinder,hoehe_zylinder,m_1)
+J_zylinder2=J_zylinder(radius_zylinder,hoehe_zylinder,m_2)
+
+print(J_zylinder1)
+
+
+#Berechnung Trägheitsmoment Zylinder grau
+schingungsdauer_grauer_zylinder*=(1/5)
+mittel_schingungsdauer_grauer_zylinder=sum(schingungsdauer_grauer_zylinder)/len(schingungsdauer_grauer_zylinder)
+mittel_schingungsdauer_grauer_zylinder_fehler= 1/(np.sqrt(len(schingungsdauer_grauer_zylinder)))*np.std(winkelricht)
+schingungsdauer_grauer_zylinder_u=ufloat(mittel_schingungsdauer_grauer_zylinder,mittel_schingungsdauer_grauer_zylinder_fehler)
+J_eigen=-(J_zylinder1+J_zylinder2)+(winkelrecht_passiv)/(4*np.pi**2)*b_fehler
+
+
+
+
+
+
+
+#Plotbereich
 plt.xlim(0,6.4)
 plt.ylim(0,52)
 x=np.linspace(0,6.5,1000)
 
 plt.plot(abstand[::3]**2,schwingungsdauer_mittel**2,'rx',label='Messwerte')
 plt.plot(x**2,m*x**2+b,'b-',label='Lineare Regression')
-#plt.plot(ab[::3]**2,f(ab[::3]**2,*params_p),'b-',label='Fit')
+plt.plot(abstand[::3]**2,f(abstand[::3]**2,*params_p),'y-',label='Fit')
 plt.legend(loc='best')
 plt.ylabel(r'$T^2 \ in \ \frac{1}{\mathrm{s}^2}$')
 plt.xlabel(r'$r^2 \ in  \ \mathrm{m}^2$')
 plt.grid()
-plt.savefig('lineare_regression.pdf')
-#plt.show()
+#plt.savefig('lineare_regression.pdf')
+plt.show()
 
 
 
