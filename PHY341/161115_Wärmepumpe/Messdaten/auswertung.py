7be8816f78d6eefab157446a0d09cfa460f29097
Stefan <stefan.grisard@tu-dortmund.de>
2016-11-14 15:16:06 +0100

    Wärmepumpe

--- PHY341/161115_Wärmepumpe/Messdaten/auswertung.py
+++ PHY341/161115_Wärmepumpe/Messdaten/auswertung.py
@@ -8,17 +8,30 @@ from uncertainties.unumpy import (nominal_values as noms,
 std_devs as stds)
 
 #Temperaturen einlesen
-T_1 = np.genfromtxt('T_1.txt', unpack = True)
-T_2 = np.genfromtxt('T_2.txt', unpack = True)
+T_error = 1
+T_1_raw = np.genfromtxt('T_1.txt', unpack = True)
+T_2_raw = np.genfromtxt('T_2.txt', unpack = True)
+T_1 = unp.uarray(T_1_raw, len(T_1) * [T_error])
+T_2 = unp.uarray(T_2_raw, len(T_2) * [T_error])
 timeinterval = 60
 t = np.linspace(0, (len(T_1) - 1) * timeinterval, num = len(T_1))
 print(t)
 #Drücke einlesen
-#p_a = np.genfromtxt('p_a.txt', unpack = True)
-#p_b = np.genfromtxt('p_b.txt', unpack = True)
+p_error = 1
+p_a_raw = np.genfromtxt('p_a.txt', unpack = True)
+p_b_raw = np.genfromtxt('p_b.txt', unpack = True)
+#Konstanten
+rho_H2O = 1
+vol_1 = 1
+vol_2 = 1
+kappa = 1.14
+# in g/dm^3, bei T = 0 und p = 1 bar
+rho_0_CI2F2C = 5.51
+
+
 
 #Massen
-m_1 = 1
+m_1 =
 m_2 = 1
 m_k = 1
 c_w = 1
@@ -68,11 +81,11 @@ def dT_dt(t, A, B):
 #für 60, 120, 180, 240
 dT1_dt = unp.uarray(np.zeros(4), np.zeros(4))
 for i in range(0,4):
-    dT1_dt[i] = dT_dt(i+1, A_F1_T1, B_F1_T1)
+    dT1_dt[i] = dT_dt(t[i+1], A_F1_T1, B_F1_T1)
 
 dT2_dt = unp.uarray(np.zeros(4), np.zeros(4))
 for i in range(0,4):
-    dT2_dt[i] = dT_dt(i+1, A_F1_T2, B_F1_T2)
+    dT2_dt[i] = dT_dt(t[i+1], A_F1_T2, B_F1_T2)
 
 dQ1_dt =  unp.uarray(np.zeros(4), np.zeros(4))
 for i in range(0,4):
@@ -81,3 +94,20 @@ for i in range(0,4):
 dQ2_dt =  unp.uarray(np.zeros(4), np.zeros(4))
 for i in range(0,4):
     dQ2_dt[i] = dT2_dt[i] * (m_2 * c_w + m_k * c_k)
+
+#hier später Berechnung von L
+L = 1
+
+#Massendurchsatz
+dm_dt = dQ2_dt / L
+
+
+#mechanische Arbeit
+#ideale Gasgleichung pV = nRT equals p = nRT/V  
+def rho(T):
+
+    return n * R * T / V
+
+N_mech = unp.uarray(np.zeros(4), np.zeros(4))
+for i in range(0,4):
+    N_mech[i] =  1/(kappa-1) * (p_b[i+1] * (p_a[i]/p_b[i+1])**(1/kappa) - p_a[i+1] ) * 1/rho_0_CI2F2C * dm_dt[i]
