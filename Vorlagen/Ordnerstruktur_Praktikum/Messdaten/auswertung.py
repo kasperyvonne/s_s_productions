bf8741c3005956cc161801dc2050a0a0cc5c8f38
Stefan <stefan.grisard@tu-dortmund.de>
2016-11-26 10:14:46 +0100

    vorlage hinzugefügt

---
+++ Vorlagen/Ordnerstruktur_Praktikum/Messdaten/auswertung.py
@@ -0,0 +1,9 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from pint import UnitRegistry
+import matplotlib.pyplot as plt
+u = UnitRegistry()
+Q_ = u.Quantity
+
+
