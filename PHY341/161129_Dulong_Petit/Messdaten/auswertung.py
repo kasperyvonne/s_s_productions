acb0aa9262ba10a66b8130f04749dce9f2229b95
Stefan <stefan.grisard@tu-dortmund.de>
2016-11-26 10:00:42 +0100

    auswertung.py dulong-petit bearbeitet

--- PHY341/161129_Dulong_Petit/Messdaten/auswertung.py
+++ PHY341/161129_Dulong_Petit/Messdaten/auswertung.py
@@ -17,3 +17,23 @@ print(c.to('joule'))
 print(a**2)
 print(b)
 ###############################
+
+#einlesen der konstanten
+rho_graphit_raw, M_graphit_raw, alpha_graphit_raw, kappa_graphit_raw = np.genfromtxt('materialkonstanten_graphit.txt', unpack=True)
+rho_graphit = Q_(rho_graphit_raw, 'gram/centimeter^3')
+M_graphit = Q_(M_graphit_raw, 'gram/mol')
+alpha_graphit = Q_(alpha_graphit_raw * 1e-06, '1/kelvin')
+kappa_graphit = Q_(kappa_graphit_raw * 1e09, 'kilogram/(second^2 meter)')
+print(rho_graphit, M_graphit, alpha_graphit, kappa_graphit)
+
+
+print(rho_graphit.to('kilogram/m^3'))
+
+#U_th in mV
+U_th = np.array(np.ones(10))
+print(U_th)
+
+def temp (U):
+    return Q_(25.157*U - 0.19* U**2, 'celsius')
+
+print(temp(U_th))
