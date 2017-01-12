527a7c819bc1d624e86a55ca846bfd95720bb5e6
Stefan <stefan.grisard@tu-dortmund.de>
2017-01-12 17:09:35 +0100

    Projekt angelegt

---
+++ PHY341/V356_Kettenschaltungen/Protokoll/Messdaten/auswertung.py
@@ -0,0 +1,37 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+import math
+import latex
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from pint import UnitRegistry
+
+u = UnitRegistry()
+Q_ = u.Quantity
+
+#variabel_1,variabel_2=np.genfromtxt('name.txt',unpack=True)
+
+
+
+
+ 
+
+
+
+
+
+#Plotbereich
+
+#plt.xlim()
+#plt.ylim()
+#aufvariabele=np.linsspace()
+#
+#plt.plot(,,'rx',label='')
+#
+#plt.grid()
+#plt.legend(loc='best')
+#plt.xlabel()
+#plt.ylabel()
+#plt.show()
+#plt.savefig('.pdf')
