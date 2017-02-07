2879d6a59e29b3ca6807be186a5eccb58cfe8aca
Stefan <stefan.grisard@tu-dortmund.de>
2017-02-07 18:04:29 +0100

    Korrekturen

--- PHY341/V354_Gedämpfte_Schwingungen/Messdaten/auswertung.py
+++ PHY341/V354_Gedämpfte_Schwingungen/Messdaten/auswertung.py
@@ -43,13 +43,20 @@ def exp_function(t, A, B, C):
     return A * np.exp(B*t) + C
 #x = np.linspace(0, 2, 100)
 ##Fits
-#params_raw, cov = curve_fit(lin_function, time, np.log(amplitude))
+
+amplitude_offset = amplitude - amplitude[-1]
+print(amplitude_offset)
+params_raw, cov = curve_fit(lin_function, time,  np.log(amplitude/amplitude[0]) )
+
+R_eff_korrektur = params_raw[0] * 2 * L
 params2_raw, cov2 = curve_fit(exp_function, time, amplitude)
 params2 = unp.uarray(params2_raw, np.sqrt(np.diag(cov2)))
-print('Parameter der linearen Rgression: ', params2)
+print('Parameter der linearen Regression: ', params2)
 print('A_0 = ', params2[0])
 mu = -1/(2*np.pi) * params2[0]
 #print(mu)
+
+print(R_eff_korrektur)
 R_eff = -(params2[1]) * 2 * L.n
 print('Effektivwiderstand: ', R_eff)
 
@@ -96,7 +103,6 @@ def phase_theo(f):
     w = 2 * np.pi * f
     return np.arctan( (-w * (R * C).n ) / (1 - (L * C).n * w**2 ) )
 
-print(U_C[9:]/U_G[9:])
 f = np.linspace(24000, 46000, 1000)
 u = np.linspace(1,4, 100)
 plt.plot(frequenz[9:]/1000, U_C[9:]/U_G[9:], 'rx', label='Messwerte')
@@ -109,6 +115,23 @@ plt.xlabel('Frequenz $\\nu$ in kHz')
 plt.ylabel('$\\frac{U_C}{U}$')
 plt.grid()
 plt.savefig('U_f_linear.pdf')
+
+plt.clf()
+f = np.linspace(9, 46000, 1000)
+u = np.linspace(1,4, 100)
+plt.plot(frequenz/1000, U_C/U_G, 'rx', label='Messwerte')
+#plt.plot(f/1000, (np.ones(len(f))* 1/np.sqrt(2) * 17/4.32), '--b', label = 'Niveaulinie $\\frac{1}{\sqrt{2}}\\frac{U_{C, max}}{U}$')
+#plt.plot(np.ones(len(u))* nu_minus_exp/1000, u, '--g', label = 'Abgelesene Frequenzen $\\nu_{\pm}$')
+#plt.plot(np.ones(len(u))* nu_plus_exp/1000, u, '--g')
+#plt.xlim (24.5, 45.5)
+plt.xlim(1*10**(-2.1) ,1*10**(2))
+plt.ylim(0.8, 4)
+plt.legend(loc='best')
+plt.xlabel('Frequenz $\\nu$ in kHz')
+plt.ylabel('$\\frac{U_C}{U}$')
+plt.xscale('log')
+plt.grid()
+plt.savefig('U_f_log.pdf')
 
 plt.clf()
 plt.plot(frequenz[9:]/1000, U_C[9:]/U_G[9:], 'rx', label='Messwerte')
@@ -135,14 +158,30 @@ plt.xlabel('Frequenz $\\nu$ in kHz')
 plt.savefig('phase_f_linear.pdf')
 
 
+lin = np.linspace(24, 46, 100)
+pha = np.linspace(0, np.pi, 100)
+plt.clf()
+plt.plot(frequenz, phase, 'rx', label='Messwerte')
+plt.yticks([0, np.pi/4, np.pi / 2, 3*np.pi/4 , np.pi],
+           [r"$0$", r"$\frac{\pi}{4}$", r"$\frac{\pi}{2}$", r"$\frac{3\pi}{4}$", r"$\pi$"], fontsize = 16)
+#plt.xlim(24.5, 45.5)
+plt.grid()
+plt.legend(loc='best')
+plt.ylabel('Phasenverschiebung $\\varphi$ in rad')
+plt.xlabel('Frequenz $\\nu$ in Hz')
+plt.ylim(-0.2, np.pi)
+plt.xscale('log')
+plt.savefig('phase_f_log.pdf')
+
+
 t = np.linspace(time[0]-0.00001, time[-1]+0.00001)
 plt.clf()
-plt.plot(t* 100000, exp_function(t, *params2_raw) , '-b', label='Fit-Funktion')
-plt.plot(time*100000, amplitude, 'rx', label = 'Messwerte')
+plt.plot(t* 100000, lin_function(t, *params_raw) , '-b', label='Fit-Funktion')
+plt.plot(time*100000, np.log(amplitude/amplitude[0]), 'rx', label = 'Messwerte')
 plt.xlim(-1, 37)
 plt.ylim()
 plt.grid()
 plt.legend(loc='best')
 plt.xlabel('Zeit t in $10^{-5}$ s')
-plt.ylabel('$A(t)$ in V')
+plt.ylabel('$log\left(\\frac{A(t)}{A_0}\\right)$')
 plt.savefig('amplitude.pdf')
