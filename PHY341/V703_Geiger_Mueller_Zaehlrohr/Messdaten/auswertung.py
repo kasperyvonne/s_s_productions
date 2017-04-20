414e92f404874e23fdb2eac18124f669889baf52
Stefan <stefan.grisard@tu-dortmund.de>
2017-04-20 14:19:29 +0200

    neues Projekt erstellt

---
+++ PHY341/V703_Geiger_Mueller_Zaehlrohr/Messdaten/auswertung.py
@@ -0,0 +1,16 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+import math
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from pint import UnitRegistry
+import latex as l
+
+u = UnitRegistry()
+Q_ = u.Quantity
+
+#umrechnung einheiten mit var.to('unit')
+# Einheiten für pint:dimensionless, meter, second, degC, kelvin
+#beispiel:
+
