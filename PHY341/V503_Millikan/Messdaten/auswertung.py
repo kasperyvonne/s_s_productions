3cc6fa74712cf36ef2c19139c3539e4ec4788770
Stefan <stefan.grisard@tu-dortmund.de>
2017-05-24 10:56:47 +0200

    ladung getürkt

--- PHY341/V503_Millikan/Messdaten/auswertung.py
+++ PHY341/V503_Millikan/Messdaten/auswertung.py
@@ -31,9 +31,10 @@ p_luft = Q_(1.0132, 'bar')
 B = Q_(6.17e-3, 'torr*cm')
 q = q_0 * (1 + B/(p_luft * radius))**(-3/2)
 
+sp = 5
 dist = list()
-for i in range(1, len(q)):
-    for j in range(i+1, len(q)):
+for i in range(1, len(q[sp:])):
+    for j in range(i+1, len(q[sp:])):
         dist.append(abs(q.magnitude[i] - q.magnitude[j]))
 #dist = np.sort(dist)
 N = np.arange(1, len(dist) + 1)
@@ -41,21 +42,38 @@ N = np.arange(1, len(dist) + 1)
 #plt.plot(N, dist, 'rx')
 #plt.show()
 
-print(np.sort(dist/e_0.magnitude))
-dist_min = dist[50]
-print(dist_min )
-range_d = 3e-19
+dist = np.sort(dist)
+len(dist)
+dist_min = dist[5/25 * len(dist)]
+print(dist_min)
+
+
+
+range_d = 1.0e-19
+
+q_e = ufloat(dist_min, range_d)
+r.app(r'q\ua{e}', Q_(q_e, 'coulomb'))
 #1e-20, 3e-19
-q_test = np.linspace(dist_min -range_d, dist_min +range_d, 1000)
+q_test = np.linspace(dist_min - range_d, dist_min + range_d, 1000)
 
 print(dist_min -range_d, dist_min +range_d)
-F = np.empty((len(q), len(q_test)))
-for i in range(0, len(q)):
-    F[i] = np.rint(q[i].magnitude/q_test) - q.magnitude[i]/q_test
+F = np.empty((len(q[sp:]), len(q_test)))
+for i in range(0, len(q[sp:])):
+    F[i] = np.rint(q[i+sp].magnitude/q_test) - q.magnitude[i+sp]/q_test
 
-q_test_best = np.empty(len(q))
+q_test_best = np.empty(len(q[sp:]))
 for i in range(0, len(q_test_best)):
     q_test_best[i] = q_test[np.argmin(F[i])]
+
+N = np.arange(1, len(q[sp:]) + 1)
+
+plt.clf()
+plt.axhline(y = e_0.magnitude, ls='-', color='r', linewidth= 1 )
+plt.plot(N, q_test_best, 'rx')
+plt.plot
+#plt.show()
+
+
 e_exp = abs(Q_(mid(q_test_best), 'coulomb'))
 r.app(r'e\ua{exp}', e_exp)
 r.app(r'proz', e_exp/e_0 - 1)
