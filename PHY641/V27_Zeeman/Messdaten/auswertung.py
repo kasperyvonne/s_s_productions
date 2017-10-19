87ece4064c20d7b6f6ccd75be31b4033993c5302
Stefan <stefan.grisard@tu-dortmund.de>
2017-10-19 19:59:19 +0200

    auswertung

--- PHY641/V27_Zeeman/Messdaten/auswertung.py
+++ PHY641/V27_Zeeman/Messdaten/auswertung.py
@@ -9,12 +9,13 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from pint import UnitRegistry
 import latex as l
-#r = l.Latexdocument('results.tex')
+r = l.Latexdocument('results.tex')
 u = UnitRegistry()
 Q_ = u.Quantity
-import pandas as pd
-from pandas import Series, DataFrame
+#import pandas as pd
+#from pandas import Series, DataFrame
 import matplotlib.image as mpimg
+import scipy.constants as const
 #series = pd.Series(data, index=index)
 # d = pd.DataFrame({'colomn': series})
 
@@ -48,7 +49,12 @@ def hysterese(I, a, b, c, d):
 #######LOAD DATA#########
 B_up, B_down = np.genfromtxt('data/hysterese.txt', unpack=True) #Flussdichte bei auf und absteigendem Spulenstrom
 I = np.linspace(0, 20, 21) # Stromstärke von 0 bis 20A in 1A Schritten
-
+l.Latexdocument('tabs/hysterese.tex').tabular(
+data = [I, B_up, B_down], #Data incl. unpuarray
+header = [r'I / \ampere', r'B\ua{auf} / \milli\tesla', r'B\ua{ab} / \milli\tesla'],
+places = [1, 0, 0],
+caption = r'Gemessene magnetische Flussdichten $B\ua{i}$ bei auf- bzw. absteigenden Spulenstrom $I$.',
+label = 'hysterese')
 
 ###FIT DER HYSTERESE######
 params_up, cov_up = curve_fit(hysterese, I, B_up)
@@ -79,18 +85,35 @@ plt.ylabel('$B/$mT')
 plt.legend()
 plt.grid()
 plt.savefig('plots/hysterese_absteigend.pdf')
-
-
-#r.makeresults()
-
-
 plt.clf()
 
-#print(img)
-plt.gray()
-img= mpimg.imread('bilder_v27/messung_3_blau_pi/IMG_0648.JPG')
-lum_img = img[:,:,1]
-plt.hist(lum_img.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
 
-#plt.imshow(img)
-plt.show()
+
+
+
+#Rechnungen mit den Positionen der Aufspaltung
+peaks_rot_0 = np.genfromtxt('data/peaks_rot_sigma_I_0.txt', unpack = True)
+peaks_rot_10 = np.genfromtxt('data/peaks_rot_sigma_I_10.txt', unpack = True)
+l.Latexdocument('tabs/peaks_rot.tex').tabular(
+data = [peaks_rot_0, unp.uarray(peaks_rot_10[:12], peaks_rot_10[12:])],
+header = ['x_0 / px', 'x_{10} / px'],
+places = [0, (4.0, 4.0)],
+caption = 'Positionen $x_0$ und $x_10$ der Intensitätsmaxima unter $I= \SI{0}{\ampere}$ und $I= \SI{10}{\ampere}$.',
+label = 'tab: peaks_rot')
+
+
+delta_s_rot = peaks_rot_0[1:] - peaks_rot_0[:-1]
+del_s_rot = (peaks_rot_10[1:] - peaks_rot_10[:-1])[::2]
+del_s_mid = [(del_s_rot[i] + del_s_rot[i+1])/2 for i in range(0, len(del_s_rot)-1)]
+lambda_rot = Q_(643.8, 'nanometer').to('meter')
+d = Q_(4, 'millimeter').to('meter')
+c = Q_(const.c, 'meter / second')
+h = Q_(const.h, 'joule * second')
+mu_B = Q_(const.physical_constants['Bohr magneton'][0], 'joule / tesla')
+n_rot = 1.4567
+del_lambda_rot = 1/2 * lambda_rot**2 / (2 * d * np.sqrt(n_rot**2 - 1) ) * (del_s_mid / delta_s_rot)
+delta_E_rot = h * c / lambda_rot**2 * del_lambda_rot
+g_rot = (delta_E_rot / (mu_B * Q_(hysterese(10, *params_up), 'millitesla'))).to('dimensionless')
+g_rot_mid = np.mean(g_rot).to('dimensionless')
+r.app(r'g\ua{r, \sigma}', Q_(ufloat(np.mean(g_rot_mid), np.std(g_rot_mid)), 'dimensionless'))
+r.makeresults()
---
+++ PHY641/V27_Zeeman/Messdaten/pictures.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.misc
+from scipy import signal
+import matplotlib.image as mpimg
+
+rot_I_0 = scipy.misc.imread('bilder_v27/messung_1_rot_sigma/b_w/m1_I_0_cut-LAB.png', flatten=True, mode=None)
+#rot_I_7_5 = scipy.misc.imread('bilder_v27/messung_1_rot_sigma/b_w/m1_I_7_5_cut-LAB.png', flatten=True, mode=None)
+rot_I_10 = scipy.misc.imread('bilder_v27/messung_1_rot_sigma/b_w/m1_I_10_cut-LAB.png', flatten=True, mode=None)
+plt.plot(range(0, 4000), rot_I_0[450], 'r-', linewidth = 0.5, label = '$I = 0$')
+#plt.plot(range(0, 4000), rot_I_7_5[450], 'b-', linewidth = 0.5)
+plt.plot(range(0, 4000), rot_I_10[450], 'b-', linewidth = 0.5, label = '$I = 10$A')
+plt.xlim(0, 4000)
+plt.ylim(60, 160)
+plt.xlabel('$x$/px')
+plt.ylabel('Helligkeit')
+plt.savefig('plots/rot_sigma_intensitaet.pdf')
+#plt.show()
+
+
+plt.clf()
+
+plt.subplot(211)
+plt.plot(range(0, 4000), rot_I_0[450], 'r-', linewidth = 0.5)
+peaks_rot_0 = np.genfromtxt('data/peaks_rot_sigma_I_0.txt', unpack = True)
+for i in peaks_rot_0:
+    plt.axvline(x = i, linewidth=0.7, color='k')
+plt.ylabel('Helligkeit')
+plt.xlabel('$x$/px')
+plt.xlim(0, 4000)
+
+plt.subplot(212)
+img = mpimg.imread('bilder_v27/messung_1_rot_sigma/m1_I_0_cut.png')
+plt.imshow(img)
+for i in peaks_rot_0:
+    plt.axvline(x = i, linewidth=1.2, color='w')
+plt.xlabel('$x$/px')
+plt.savefig('plots/peaks_rot_sigma_0.pdf')
+
+plt.clf()
+plt.subplot(211)
+plt.plot(range(0, 4000), rot_I_10[450], 'r-', linewidth = 0.5)
+peaks_rot_10 = np.genfromtxt('data/peaks_rot_sigma_I_10.txt', unpack = True)
+for i in peaks_rot_10:
+    plt.axvline(x = i, linewidth=0.7, color='k')
+plt.xlim(0, 4000)
+plt.ylabel('Helligkeit')
+plt.xlabel('$x$/px')
+
+plt.subplot(212)
+img = mpimg.imread('bilder_v27/messung_1_rot_sigma/m1_I_10_cut.png')
+plt.imshow(img)
+for i in peaks_rot_10:
+    plt.axvline(x = i, linewidth=1.2, color='w')
+plt.ylabel('Helligkeit')
+plt.xlabel('$x$/px')
+plt.savefig('plots/peaks_rot_sigma_10.pdf')
