78abb22a728b262c5d440ed2b86845a310df88df
Stefan <stefan.grisard@tu-dortmund.de>
2017-11-12 09:33:31 +0100

    plot p(t) hinzugefügt

--- PHY641/V70_Vakuumphysik/Protokoll/theorie_plots/plot.py
+++ PHY641/V70_Vakuumphysik/Protokoll/theorie_plots/plot.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 t = np.linspace(0, 10, 100)
-plt.plot(t, (2-1)*np.exp(-t) + 1)
-plt.axhline(y = 1, color = 'k', linestyle = '--')
-plt.yticks([1, 2], ['$p_G$', '$p_0$'])
-plt.xlabel(r'$\frac{S}{V}t$')
+p_0 = 5
+p_g = 1
+
+plt.plot(t, (p_0-p_g)*np.exp(-t) + p_g, 'r-',label = 'Druckverlauf $p(t)$')
+plt.axhline(y = p_g, color = 'k', linestyle = '--')
+plt.yticks([p_g, p_0], ['$p_G$', '$p_0$'])
+plt.xlabel(r'$\frac{S}{V}\,t$')
 plt.ylabel(r'$p$')
+plt.ylim(0, p_0)
+plt.xlim(0, 10)
+plt.legend()
 plt.savefig('theo_p.pdf')
