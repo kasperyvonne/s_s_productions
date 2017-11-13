63ffa345f774d66fff1d6ca9af9b07a64a692d71
Stefan <stefan.grisard@tu-dortmund.de>
2017-11-13 11:41:40 +0100

    Ergänzungen, Plot p(t) bearbeitet

--- PHY641/V70_Vakuumphysik/Protokoll/theorie_plots/plot.py
+++ PHY641/V70_Vakuumphysik/Protokoll/theorie_plots/plot.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-t = np.linspace(0, 10, 100)
+t = np.linspace(-0.5, 10, 100)
 p_0 = 5
 p_g = 1
 
 plt.plot(t, (p_0-p_g)*np.exp(-t) + p_g, 'r-',label = 'Druckverlauf $p(t)$')
 plt.axhline(y = p_g, color = 'k', linestyle = '--')
 plt.yticks([p_g, p_0], ['$p_G$', '$p_0$'])
-plt.xlabel(r'$\frac{S}{V}\,t$')
-plt.ylabel(r'$p$')
-plt.ylim(0, p_0)
-plt.xlim(0, 10)
+plt.xlabel(r'$\frac{S}{V}\,t$', fontsize = 12)
+plt.ylabel(r'$p$', fontsize = 12)
+plt.ylim(0, p_0 + 0.5)
+plt.xlim(-0.5, 7)
 plt.legend()
 plt.savefig('theo_p.pdf')
