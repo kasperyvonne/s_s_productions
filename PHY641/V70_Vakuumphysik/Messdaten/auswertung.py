1080dbcc0e88dca9fe6c4071f9f9f4469f9a3819
beckstev <beckstev@web.de>
2017-11-15 22:32:41 +0100

    started with the auswertung

--- PHY641/V70_Vakuumphysik/Messdaten/auswertung.py
+++ PHY641/V70_Vakuumphysik/Messdaten/auswertung.py
@@ -8,6 +8,7 @@ import math
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from pint import UnitRegistry
+from scipy.stats import sem
 import latex as l
 #r = l.Latexdocument('results.tex')
 u = UnitRegistry()
@@ -34,4 +35,49 @@ caption = 'testcaption.',
 label = 'testlabel')
 ##############################################
 
-#r.makeresults()
+
+## Funktionen ##
+
+def mittelwert_zeit(t_1,t_2,t_3,t_4,t_5): ## Funktion um die Zeitmittelwerte der Messreihe zu bestimmen.
+    n=len(t_1)
+    mittel=[]
+    fehler=[]
+
+    for i in range(n):
+        zeit= [ t_1[i], t_2[i], t_3[i], t_4[i], t_5[i] ]
+        mittel.append(np.mean(zeit))
+        fehler.append(sem(zeit))
+
+    return unp.uarray(mittel,fehler)
+
+
+
+
+
+
+
+###  Drehschieber
+#Konstanten
+enddruck=ufloat(1e-2, 1e-2*0.2)
+
+
+p_druck, t_ddruck_1, t_ddruck_2, t_ddruck3, t_ddruck4, t_ddruck5 = np.genfromtxt('./messdaten/drehshieber/drehschieber_druck.txt', unpack=True)
+
+preasure_druck=unp.uarray(p_druck,p_druck*0.2)
+
+zeiten_dreh_druck_gemittelt=mittelwert_zeit( t_ddruck_1, t_ddruck_2, t_ddruck3, t_ddruck4, t_ddruck5)
+
+lograritmierter_druck=unp.log( (preasure_druck-enddruck )/ (preasure_druck[0]-enddruck) )
+
+
+
+## Druckkurve
+
+## Leckkurve
+
+
+###  Turbo
+
+## Druckkurve
+
+## Leckkurve
