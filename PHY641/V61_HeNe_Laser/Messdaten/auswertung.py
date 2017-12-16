4a64f25a5c9608a02154bb1fee1923838301dd2b
Stefan <stefan.grisard@tu-dortmund.de>
2017-12-16 10:49:45 +0100

    neues projekt V61 erstellt

---
+++ PHY641/V61_HeNe_Laser/Messdaten/auswertung.py
@@ -0,0 +1,37 @@
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import nominal_values as noms
+from uncertainties.unumpy import std_devs as stds
+from uncertainties import correlated_values
+import math
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from pint import UnitRegistry
+import latex as l
+#r = l.Latexdocument('results.tex')
+u = UnitRegistry()
+Q_ = u.Quantity
+import pandas as pd
+from pandas import Series, DataFrame
+#series = pd.Series(data, index=index)
+# d = pd.DataFrame({'colomn': series})
+
+#############################################
+#EXAMPLE FOR LATEX TABLE WITH UNP ARRAYS
+a = [1.1, 2.2, 3.3]
+b = [0.1, 0.2, 0.3]
+c = [3, 4, 5]
+d = [6, 7, 8] #EXAMPLE ARRAYS
+
+f = unp.uarray(a, b) #EXAMPLE UARRAY
+
+l.Latexdocument('latex_example.tex').tabular(
+data = [c, d, f], #Data incl. unpuarray
+header = ['\Delta Q / \giga\elementarycharge', 'T_1 / \micro\second', 'T_1 / \micro\second'],
+places = [1, 1, (1.1, 1.1)],
+caption = 'testcaption.',
+label = 'testlabel')
+##############################################
+
+#r.makeresults()
